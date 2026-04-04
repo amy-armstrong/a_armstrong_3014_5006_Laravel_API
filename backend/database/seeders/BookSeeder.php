d6b8dac9ef81c61fd6ac5f3746acf88b89b38a1d
amy-armstrong <a_armstrong148615@fanshaweonline.ca>
2026-04-03 23:29:20 -0400

    bookseeder updated
    added books to seeder

--- backend/database/seeders/BookSeeder.php
+++ backend/database/seeders/BookSeeder.php
@@ -15,5 +15,26 @@ class BookSeeder extends Seeder
             'published_year' => 1925,
             'description' => 'A story of wealth and love.'
         ]);
+
+        Book::create([
+            'title' => "Harry Potter and the Philosopher's Stone",
+            'author_id' => 1,
+            'published_year' => 1997,
+            'description' => 'The boy who lived starts his journey at Hogwarts.'
+        ]);
+
+        Book::create([
+            'title' => "Harry Potter and the Chamber of Secrets",
+            'author_id' => 1,
+            'published_year' => 1998,
+            'description' => 'A mysterious diary and a giant snake.'
+        ]);
+
+        Book::create([
+            'title' => "The Great Gatsby",
+            'author_id' => 2,
+            'published_year' => 1925,
+            'description' => 'A classic tale of the American Dream.'
+        ]);
     }
 }
